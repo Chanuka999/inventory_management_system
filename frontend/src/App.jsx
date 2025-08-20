d3738a336a9fefcec234e3bd1f6552e0763f161a
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-20 23:57:25 +0530

    feat: update routing in App component to include Root and Login pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Root from "./components/Root";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline text-red-500">
+    <div className="">
       <Router>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/contact" element={<h1>Contact</h1>} />
+          <Route path="/" element={<Root />} />
+          <Route path="/admin/dashboard" element={<h1>Admin dashboard</h1>} />
+          <Route
+            path="/customer/dashboard"
+            element={<h1>Customer dashboard</h1>}
+          />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
