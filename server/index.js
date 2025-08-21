36e1da3b50fcb9e0d77f83c9a052ef541e67e124
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-21 21:42:14 +0530

    update

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDb from "./db/connection.js";
-import authRoutes from "./models/user.js";
+import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
