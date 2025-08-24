a9679884fd953b9cb68108afdcb71cd2e81d9781
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-24 22:49:17 +0530

    feat: implement getCategories and updateCategory functions in categoryController

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -25,4 +25,42 @@ const addCategory = async (req, res) => {
   }
 };
 
-export default addCategory;
+const getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    return res.status(200).json({ success: true, categories });
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "server error in getting" });
+  }
+};
+
+const updateCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { categoryName, categoryDescription } = req.body;
+
+    const existingCategory = await Category.findById(id);
+    if (!existingCategory) {
+      return res
+        .status(404)
+        .json({ success: false, message: "category not found" });
+    }
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      id,
+      { categoryName, categoryDescription },
+      { new: true }
+    );
+    return res
+      .status(200)
+      .json({ success: true, message: "category updated successfully" });
+  } catch (error) {
+    console.error("Error updating category", error);
+    return res.status(500).json({ success: false, message: "server error" });
+  }
+};
+
+export { addCategory, getCategories, updateCategory };
