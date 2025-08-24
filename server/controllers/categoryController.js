72ebadf26d2a993a13071bee682c620ade41177f
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-24 22:59:17 +0530

    feat: implement deleteCategory function in categoryController for category removal

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -63,4 +63,26 @@ const updateCategory = async (req, res) => {
   }
 };
 
-export { addCategory, getCategories, updateCategory };
+const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const existingCategory = await Category.findById(id);
+
+    if (!existingCategory) {
+      return res
+        .status(400)
+        .json({ success: false, message: "category not found" });
+    }
+
+    await Category.findByIdAndDelete(id);
+    return res
+      .status(200)
+      .json({ success: true, message: "category deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting category", error);
+    return res.status(500).json({ success: false, message: "sercer error" });
+  }
+};
+
+export { addCategory, getCategories, updateCategory, deleteCategory };
