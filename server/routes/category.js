cbe87980dab6ec646834fbfbf2f6b4b725596a25
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-24 22:49:28 +0530

    feat: add authMiddleware to category routes for enhanced security

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,7 +1,14 @@
 import express from "express";
-import addCategory from "../controllers/categoryController.js";
+import {
+  addCategory,
+  getCategories,
+  updateCategory,
+} from "../controllers/categoryController.js";
+import authMiddleware from "../middlewhere/authMiddleware.js";
 const router = express();
 
-router.post("/add", addCategory);
+router.post("/add", authMiddleware, addCategory);
+router.get("/", authMiddleware, getCategories);
+router.put("/:id", authMiddleware, updateCategory);
 
 export default router;
