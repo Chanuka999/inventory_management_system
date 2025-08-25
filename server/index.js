0961497369f30d3b0f8c36049fc25f1e0b53cddd
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-25 19:54:09 +0530

    addd

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDb from "./db/connection.js";
 import authRoutes from "./routes/auth.js";
 import categoryRoutes from "./routes/category.js";
+import supplierRoutes from "./routes/suplier.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/category", categoryRoutes);
+app.use("/api/supplier", supplierRoutes);
 
 app.listen(PORT, () => {
   connectDb();
