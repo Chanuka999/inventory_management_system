4d6a4b070d1b283f203d33b77c5acb7ebc2d8e96
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-26 22:14:16 +0530

    feat: enable fetching suppliers by adding getSuppliers route

--- server/routes/suplier.js
+++ server/routes/suplier.js
@@ -1,10 +1,13 @@
 import express from "express";
 import authMiddleware from "../middlewhere/authMiddleware.js";
-import { addSupplier } from "../controllers/supplierController.js";
+import {
+  addSupplier,
+  getSuppliers,
+} from "../controllers/supplierController.js";
 const router = express();
 
 router.post("/add", authMiddleware, addSupplier);
-// router.get("/", authMiddleware, getCategories);
+router.get("/", authMiddleware, getSuppliers);
 // router.put("/:id", authMiddleware, updateCategory);
 // router.delete("/:id", authMiddleware, deleteCategory);
 
