426dae931a2ba2038ad1be2c73e83c4c8da60b19
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-27 01:19:13 +0530

    feat: implement supplier fetching and management in Supliers component

--- frontend/src/components/Supliers.jsx
+++ frontend/src/components/Supliers.jsx
@@ -5,6 +5,8 @@ import { useEffect } from "react";
 const Supliers = () => {
   const [addEditModel, setAddEditModel] = useState(null);
   const [loading, setLoading] = useState(null);
+  const [supliers, setSupliers] = useState([]);
+  const [editSupplier,setSupplier] = useState(null);
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -23,14 +25,13 @@ const Supliers = () => {
   const fetchCategories = async () => {
     setLoading(true);
     try {
-      const responce = await axios.get("http://localhost:5000/api/category", {
+      const responce = await axios.get("http://localhost:5000/api/supplier", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("pos-token")}`,
         },
       });
-      console.log(responce.data.categories);
-      setCategories(responce.data.categories);
-      setLoading(false);
+
+      setSupliers(responce.data.supliers);
     } catch (error) {
       console.error("Error fetching categories", error);
       setLoading(false);
@@ -60,12 +61,6 @@ const Supliers = () => {
       if (response.data.success) {
         alert("supplier added successfully");
         setAddEditModel(null);
-        setFormData({
-          name: "",
-          email: "",
-          phone: "",
-          address: "",
-        });
       } else {
         console.error("Error editing suplier:", response.data);
         alert("error editing suplier.please try again.");
@@ -74,6 +69,27 @@ const Supliers = () => {
       console.error("Error adding suplier ", error);
       alert("Error adding supplier,please try again");
     }
+  };
+
+  const handdleEdit = (suplier) => {
+    setFormData({
+      name: suplier.name,
+      email: suplier.email,
+      number: suplier.number,
+      address: suplier.address,
+    });
+    setAddEditModel(suplier._id);
+  };
+
+  const closeModal = () => {
+    setAddEditModel(null);
+    setFormData({
+      name: "",
+      email: "",
+      phone: "",
+      address: "",
+    });
+    setEditSupplier(true);
   };
   return (
     <div className="w-full h-full flex-col gap-4 p-4">
@@ -86,11 +102,53 @@ const Supliers = () => {
         />
         <button
           className="px-4 py-1.5 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-900"
-          onClick={() => setAddEditModel(1)}
+          onClick{()=> setAddEditModel}
         >
           Add Suplier
         </button>
       </div>
+
+      {loading ? (
+        <div>Loading ....</div>
+      ) : (
+        <table className="w-full border-collapse border border-gray-300 mt-4">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="border border-gray-300 p-2">S No</th>
+              <th className="border border-gray-300 p-2">Supplier name</th>
+              <th className="border border-gray-300 p-2">Email</th>
+              <th className="border border-gray-300 p-2">Phone number</th>
+              <th className="border border-gray-300 p-2">Address</th>
+              <th className="border border-gray-300 p-2">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {supliers.map((suplier, index) => (
+              <tr key={suplier._id}>
+                <td className="border border-gray-300 p-2">{index + 1}</td>
+                <td className="border border-gray-300 p-2">{suplier.name}</td>
+                <td className="border border-gray-300 p-2">{suplier.email}</td>
+                <td className="border border-gray-300 p-2">{suplier.number}</td>
+                <td className="border border-gray-300 p-2">
+                  {suplier.address}
+                </td>
+                <td className="border border-gray-300 p-2">
+                  <button
+                    className="px-2 py-1 bg-yellow-500 text-white rounded cursor-pointer mr-2"
+                    onClick={handdleEdit}
+                  >
+                    Edit
+                  </button>
+                  <button className="px-2 py-1 bg-red-500 text-white rounded cursor-pointer">
+                    delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
       {addEditModel && (
         <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center">
           <div className="bg-white p-4 rounded shadow-md w-1/3 relative">
@@ -134,9 +192,25 @@ const Supliers = () => {
                 placeholder="Supplier address"
                 className="border p-1 bg-white rounded px-4"
               />
-              <button className="px-4 py-1.5 bg-blue-500 text-white rounded cursor-pointer  hover:bg-blue-900">
-                Add Suplier
-              </button>
+
+              <div className="flex space-x-2">
+                <button
+                  type="submit"
+                  className="w-full rounded-md bg-green-500 text-white p-3 cursor-pointer hover:bg-green-800"
+                >
+                  {addEditModel ? "save changes" : "Add supplier"}
+                </button>
+
+                {addEditModel && (
+                  <button
+                    type="button"
+                    className="w-full mt-2 rounded-md bg-red-500 text-white p-3 cursor-pointer hover:bg-red-600"
+                    onClick={closeModal}
+                  >
+                    Cancel
+                  </button>
+                )}
+              </div>
             </form>
           </div>
         </div>
