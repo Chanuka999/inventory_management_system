9a93a7762119a20086b0afacae3420dc4de4063e
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-28 01:17:03 +0530

    feat: add Product component for supplier management

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className="w-full h-full flex flex-col gap-4 p-4">
+      <h1 className="text-2xl font-bold">Suplier Management</h1>
+      <div className="flex justify-between items-center">
+        <input
+          type="text "
+          placeholder="serch"
+          className="border p-1 bg-white rounded px-4"
+          // onChange={handdleSearch}
+        />
+        <button
+          className="px-4 py-1.5 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-900"
+          //onClick={() => setAddModel(true)}
+        >
+          Add Suplier
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
