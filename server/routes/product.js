f646da014f8340b5eeae632baa433bd4da6a344f
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-28 22:01:07 +0530

    feat: update product routes to include addProduct handler

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,11 +1,11 @@
 import express from "express";
 import authMiddleware from "../middlewhere/authMiddleware.js";
-import { getProducts } from "../controllers/productController.js";
+import { getProducts, addProduct } from "../controllers/productController.js";
 
 const router = express();
 
 router.get("/", authMiddleware, getProducts);
-// router.post("/add", authMiddleware, addSupplier);
+router.post("/add", authMiddleware, addProduct);
 // router.put("/:id", authMiddleware, updateSupplier);
 // router.delete("/:id", authMiddleware, deleteSupplier);
 
