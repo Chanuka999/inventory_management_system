c74570592f2b2e731f53a280754f8ddd5a1b22c6
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-28 22:00:42 +0530

    feat: add form handling for supplier management and fetch categories

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -5,6 +5,13 @@ const Product = () => {
   const [opentModel, setOpenModel] = useState(false);
   const [categories, setCategories] = useState([]);
   const [suppliers, setSuppliers] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    price: "",
+    stock: "",
+    categoryId: "",
+    supplierId: "",
+  });
 
   const fetchProducts = async () => {
     try {
@@ -13,17 +20,74 @@ const Product = () => {
           Authorization: `Bearer ${localStorage.getItem("pos-token")}`,
         },
       });
-
       setSuppliers(responce.data.supliers);
-      setCategories(responce.data.supliers);
     } catch (error) {
-      console.error("Error fetching suppli ers", error);
+      console.error("Error fetching suppliers", error);
+    }
+  };
+
+  const fetchCategories = async () => {
+    try {
+      const responce = await axios.get("http://localhost:5000/api/category", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("pos-token")}`,
+        },
+      });
+      setCategories(responce.data.categories);
+    } catch (error) {
+      console.error("Error fetching categories", error);
     }
   };
 
   useEffect(() => {
     fetchProducts();
+    fetchCategories();
   }, []);
+
+  const handdleChange = (e) => {
+    const [name, value] = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+    const handdleSubmit = async (e) => {
+    e.preventDefault();
+   
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/api/products/add",
+
+          formData,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("pos-token")}`,
+            },
+          }
+        );
+
+        if (response.data.success) {
+          //fetchSuppliers();
+          alert("Product added successfully");
+          opentModel(false)
+          setAddModel(false);
+          setFormData({
+            name: "",
+            email: "",
+            phone: "",
+            address: "",
+          });
+        } else {
+          //  console.error("Error editing suplier:", response.data);
+          alert("error adding products.please try again.");
+        }
+      } catch (error) {
+        console.error("Error adding suplier ", error);
+        alert("Error adding supplier,please try again");
+      }
+    }
+  };
   return (
     <div className="w-full h-full flex flex-col gap-4 p-4">
       <h1 className="text-2xl font-bold">Suplier Management</h1>
@@ -52,52 +116,60 @@ const Product = () => {
             >
               X
             </button>
-            <form className="flex flex-col gap-4 mt-4">
+            <form className="flex flex-col gap-4 mt-4" onSubmit={handdleSubmit}>
               <input
                 type="text"
                 name="name"
-                // value={formData.name}
-                // onChange={handdleChange}
-                placeholder="Supplier name"
+                value={formData.name}
+                onChange={handdleChange}
+                placeholder="Product name"
                 className="border p-1 bg-white rounded px-4"
               />
               <input
                 type="text"
                 placeholder="Description"
                 name="description"
-                // value={formData.email}
-                // onChange={handdleChange}
+                value={formData.email}
+                onChange={handdleChange}
                 className="border p-1 bg-white rounded px-4"
               />
               <input
                 type="number"
                 name="price"
-                // value={formData.number}
-                // onChange={handdleChange}
-                placeholder="Supplier Address"
+                value={formData.number}
+                onChange={handdleChange}
+                // placeholder="Supplier Address"
                 className="border p-1 bg-white rounded px-4"
               />
               <input
                 type="number"
                 name="stock"
                 // value={formData.address}
-                // onChange={handdleChange}
+                onChange={handdleChange}
                 placeholder="Enter stock"
                 className="border p-1 bg-white rounded px-4"
               />
 
-              <div>
-                <select name="category">
+              <div className="w-full border">
+                <select name="category" className="w-full p-2">
                   <option value="">Select Category</option>
+                  {categories &&
+                    categories.map((category) => (
+                      <option key={category._id} value={category._id}>
+                        {category.categoryName}
+                      </option>
+                    ))}
                 </select>
               </div>
 
-              <div>
-                <select name="supplier">
+              <div className="w-full border">
+                <select name="supplier" className="w-full p-2">
                   <option value="">Select supplier</option>
-                  {categories &&
-                    categories.map((category) => (
-                      <option key={category._id}></option>
+                  {suppliers &&
+                    suppliers.map((suplier) => (
+                      <option key={suplier._id} value={suplier._id}>
+                        {suplier.name}
+                      </option>
                     ))}
                 </select>
               </div>
@@ -107,18 +179,16 @@ const Product = () => {
                   type="submit"
                   className="w-full rounded-md bg-green-500 text-white p-3 cursor-pointer hover:bg-green-800"
                 >
-                  {editSupplier ? "save changes" : "Add supplier"}
+                  Add Prouct
                 </button>
 
-                {editSupplier && (
-                  <button
-                    type="button"
-                    className="w-full mt-2 rounded-md bg-red-500 text-white p-3 cursor-pointer hover:bg-red-600"
-                    onClick={closeModal}
-                  >
-                    Cancel
-                  </button>
-                )}
+                <button
+                  type="button"
+                  className="w-full mt-2 rounded-md bg-red-500 text-white p-3 cursor-pointer hover:bg-red-600"
+                  onClick={() => setOpenModel(false)}
+                >
+                  Cancel
+                </button>
               </div>
             </form>
           </div>
