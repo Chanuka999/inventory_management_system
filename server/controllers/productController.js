45b16a05dd76874d4442f5bee99f6a99b7f17f02
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-29 22:26:20 +0530

    feat: implement addProduct function to handle product creation

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,30 @@
 import Supplier from "../models/Suplier.js";
 import Category from "../models/Category.js";
+import Product from "../models/Product.js";
+
+const addProduct = async (req, res) => {
+  try {
+    const { name, description, price, stock, categoryId, supplierId } =
+      req.body;
+
+    const newProduct = new Product({
+      name,
+      description,
+      price,
+      stock,
+      categoryId,
+      supplierId,
+    });
+
+    await newProduct.save();
+    return res
+      .status(200)
+      .json({ success: true, message: "Product added successfully" });
+  } catch (error) {
+    console.error("Error adding Product:", error);
+    return res.status(500).json({ success: false, message: "Server error" });
+  }
+};
 
 const getProducts = async (req, res) => {
   try {
@@ -14,4 +39,4 @@ const getProducts = async (req, res) => {
   }
 };
 
-export { getProducts };
+export { getProducts, addProduct };
