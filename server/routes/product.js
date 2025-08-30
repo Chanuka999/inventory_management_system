3397a05130fa9a9e497a242e047d8bc08cfd77b9
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-30 14:01:39 +0530

    feat: add update and delete routes for product management

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,12 +1,17 @@
 import express from "express";
 import authMiddleware from "../middlewhere/authMiddleware.js";
-import { getProducts, addProduct } from "../controllers/productController.js";
+import {
+  getProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productController.js";
 
 const router = express();
 
 router.get("/", authMiddleware, getProducts);
 router.post("/add", authMiddleware, addProduct);
-// router.put("/:id", authMiddleware, updateSupplier);
-// router.delete("/:id", authMiddleware, deleteSupplier);
+router.put("/:id", authMiddleware, updateProduct);
+router.delete("/:id", authMiddleware, deleteProduct);
 
 export default router;
