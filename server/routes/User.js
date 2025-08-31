a56b1babeded5baa36813e37e0bc7fe89bd094f2
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-31 16:52:32 +0530

    feat: update user routes to include getUser and deleteUser functionality

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,11 +1,10 @@
 import express from "express";
-
+import { addUser, getUser, deleteUser } from "../controllers/userController.js";
 import authMiddleware from "../middlewhere/authMiddleware.js";
 const router = express();
 
 router.post("/add", authMiddleware, addUser);
-// router.get("/", authMiddleware, getCategories);
-// router.put("/:id", authMiddleware, updateCategory);
-// router.delete("/:id", authMiddleware, deleteCategory);
+router.get("/", authMiddleware, getUser);
+router.delete("/:id", authMiddleware, deleteUser);
 
 export default router;
