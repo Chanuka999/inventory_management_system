fd5742c416f45104f1cd9f175f1a7ef5195bea84
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-06 11:53:36 +0530

    feat: implement order placement modal in CustomerProducts component

--- frontend/src/components/CustomerProducts.jsx
+++ frontend/src/components/CustomerProducts.jsx
@@ -7,6 +7,14 @@ const CustomerProducts = () => {
 
   const [products, setProducts] = useState([]);
   const [filterProducts, setFilterProducts] = useState([]);
+  const [opentModel, setOpenModel] = useState(false);
+  const [orderData, setOderData] = useState({
+    productId: "",
+    quantity: 1,
+    total: 0,
+    stock: 0,
+    price: 0,
+  });
 
   const fetchProducts = async () => {
     try {
@@ -60,6 +68,60 @@ const CustomerProducts = () => {
           return false;
         })
       );
+    }
+  };
+
+  const handdleOrderChange = (product) => {
+    setOderData({
+      productId: product._id,
+      quantity: 1,
+      total: product.total,
+      stock: product.stock,
+      price: product.price,
+    });
+    setOpenModel(true);
+  };
+
+  const closeModel = () => {
+    setOpenModel(false);
+  };
+
+  const handdleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const responce = await axios.post(
+        "http://localhost:5000/api/products",
+        orderData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("pos-token")}`,
+          },
+        }
+      );
+      if (responce.data.success) {
+        setOpenModel(false);
+        setOderData({
+          productId: "",
+          quantity: 1,
+          stock: 0,
+          total: 0,
+          price: 0,
+        });
+      }
+    } catch (error) {
+      alert("Error", error.message);
+    }
+  };
+
+  const increaseQuntity = (e) => {
+    if (e.target.value > orderData.stock) {
+      alert("Not enought stock");
+    } else {
+      setOderData((prev) => ({
+        ...prev,
+        quantity: parseInt(e.target.value),
+        total: parseInt(e.target.value) * parseInt(orderData.price),
+      }));
     }
   };
   return (
@@ -132,7 +194,10 @@ const CustomerProducts = () => {
                     </span>
                   </td>
                   <td className="border border-gray-300 p-2">
-                    <button className="px-2 py-1 bg-green-500  hover:bg-green-700 text-white rounded cursor-pointer mr-2">
+                    <button
+                      onClick={() => handdleOrderChange(product)}
+                      className="px-2 py-1 bg-green-500  hover:bg-green-700 text-white rounded cursor-pointer mr-2"
+                    >
                       Order
                     </button>
                   </td>
@@ -142,6 +207,49 @@ const CustomerProducts = () => {
         </table>
         {filterProducts.length === 0 && <div>No record</div>}
       </div>
+      {opentModel && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center z-20">
+          <div className="bg-white p-4 rounded shadow-md w-1/3 relative">
+            <h1 className="text-xl font-bold">place order</h1>
+            <button
+              className="absolute top-4 right-4 font-bold text-lg cursor-pointer"
+              onClick={closeModel}
+            >
+              X
+            </button>
+            <form className="flex flex-col gap-4 mt-4" onSubmit={handdleSubmit}>
+              <input
+                type="number"
+                name="quantity"
+                value={orderData.quantity}
+                onChange={increaseQuntity}
+                min="1"
+                placeholder="increase quantity"
+                className="border p-1 bg-white rounded px-4"
+              />
+
+              <p>{orderData.quantity * orderData.price}</p>
+
+              <div className="flex space-x-2">
+                <button
+                  type="submit"
+                  className="w-full rounded-md bg-green-500 text-white p-3 cursor-pointer hover:bg-green-800"
+                >
+                  "save changes"
+                </button>
+
+                <button
+                  type="button"
+                  className="w-full mt-2 rounded-md bg-red-500 text-white p-3 cursor-pointer hover:bg-red-600"
+                  onClick={closeModel}
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
