a476cadca45bdad149671a4f4ed8fa93720a9345
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-06 19:14:07 +0530

    feat: add GET route for retrieving orders in order management

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,9 +1,10 @@
 import express from "express";
 import authMiddleware from "../middlewhere/authMiddleware.js";
-import { addOrder } from "../controllers/orderController.js";
+import { addOrder, getOrders } from "../controllers/orderController.js";
 
 const router = express();
 
 router.post("/add", authMiddleware, addOrder);
+router.get("/", authMiddleware, getOrders);
 
 export default router;
