52392d149a15f935ac3a295f03476d9f2570961b
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-16 00:34:17 +0530

    add: implement getData function to aggregate product and order statistics

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,3 +1,26 @@
-const getData = async (req, res) => {};
+import orderModel from "../models/Order.js";
+import Product from "../models/Product.js";
+
+const getData = async (req, res) => {
+  const totalProducts = await Product.countDocuments();
+
+  const stockResult = await Product.aggregate([
+    { $group: { _id: null, totalStock: { $sum: "$stock" } } },
+  ]);
+  const totalStock = stockResult[0]?.totalStock || 0;
+  const startOfDay = new Date();
+  startOfDay.setHours(0, 0, 0, 0);
+  const endOfDay = new Date();
+  endOfDay.setHours(23, 59, 59, 999);
+  const ordersToday = await orderModel.countDocuments({
+    orderDate: { $gte: startOfDay, $lte: endOfDay },
+  });
+  const revenueResult = await orderModel.aggregate([
+    { $group: { _id: null, totalRevenue: { $sum: "$totalPrice" } } },
+  ]);
+  const revenue = revenueResult?.totalStock || 0;
+
+  const outOfstock = await Product.find({ stock: 0 }).select("name:stock");
+};
 
 export { getData };
